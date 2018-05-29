d74ecbcf801abe8b70a37b453551538f75d46dd3
SeoSeongHo <swa04246@naver.com>
2018-05-29 10:35:39 +0900

    loginView design

--- src/modules/login/loginView.js
+++ src/modules/login/loginView.js
@@ -1,7 +1,14 @@
 // @flow
 import React, { Component } from 'react'
-import { Input, Alert, Button, Container, Row, Col } from 'reactstrap'
+import { Input, Alert, Button, Container, Row, Col, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, CardDeck, Form, FormGroup, Label,   Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+Jumbotron,} from 'reactstrap'
 import autoBind from 'react-autobind'
+import './loginView.css'
 import eyeSlash from 'react-icons/lib/fa/eye-slash'
 import eye from 'react-icons/lib/fa/eye'
 import { Link, browserHistory } from 'react-router'
@@ -10,6 +17,23 @@ import { Link, browserHistory } from 'react-router'
 import {
   withRouter
 } from 'react-router-dom'
+
+// const items = [
+//   {
+//     id: 1,
+//     // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+//     altText: 'Slide 1',
+//     caption: 'Slide 1'
+//   },
+//   {
+//     id: 2,
+//     // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+//     altText: 'Slide 2',
+//     caption: 'Slide 2'
+//   },
+//   {
+//     id: 3,
+//     // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%2
 
 type State = {
   username: string,
@@ -26,6 +50,12 @@ type Props = {
 class LoginView extends Component<Props, State> {
   constructor(props,context) {
     super(...arguments);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
     autoBind(this);
   }
 
@@ -69,18 +99,111 @@ class LoginView extends Component<Props, State> {
       null
     )
   }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col>
-            <Input placeholder="USERNAME" onChange={this.onChangeUsername} />
+      <Container className="cont">
+        <Row className="ro1">
+          <Col className="main1" xs="3" sm="4">
+            <Jumbotron>
+              <Container>
+                <h1 className="display-3">Welcome to here!</h1>
+                <p className="lead">Space to share your pet's information and manage your pet's daily.</p>
+              </Container>
+            </Jumbotron>
+            {/*<style>*/}
+              {/*{*/}
+                {/*`.custom-tag {*/}
+                {/*max-width: 100%;*/}
+                {/*height: 500px;*/}
+                {/*background: black;*/}
+              {/*}`*/}
+              {/*}*/}
+            {/*</style>*/}
+            {/*<Carousel*/}
+              {/*activeIndex={activeIndex}*/}
+              {/*next={this.next}*/}
+              {/*previous={this.previous}*/}
+            {/*>*/}
+              {/*<CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />*/}
+              {/*{slides}*/}
+              {/*<CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />*/}
+              {/*<CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />*/}
+            {/*</Carousel>*/}
+          {/*<Carousel*/}
+            {/*activeIndex={activeIndex}*/}
+            {/*next={this.next}*/}
+            {/*previous={this.previous}*/}
+          {/*>*/}
+            {/*<CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />*/}
+            {/*{slides}*/}
+            {/*<CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />*/}
+            {/*<CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />*/}
+          {/*</Carousel>*/}
           </Col>
-          <Col>
-            <Input type="password" placeholder="PASSWORD" onChange={this.onChangePassword} />
-          </Col>
-          <Col>
-            <Button onClick={() => this.onLoginPressed()}>LOGIN</Button>
+          {/*<Col>*/}
+            {/*<Input placeholder="USERNAME" onChange={this.onChangeUsername} />*/}
+          {/*</Col>*/}
+          {/*<Col>*/}
+            {/*<Input type="password" placeholder="PASSWORD" onChange={this.onChangePassword} />*/}
+          {/*</Col>*/}
+          {/*<Col>*/}
+            {/*<Button onClick={() => this.onLoginPressed()}>LOGIN</Button>*/}
+          {/*</Col>*/}
+          <Col className="main1" xs="3" sm="4">
+          <Card body outline color="secondary">
+            <CardBody>
+              <CardTitle>Petstagram</CardTitle>
+            </CardBody>
+            <img width="100%" src={require('../../assets/images/example1.png')} alt="Card image cap" />
+            <CardBody className="card3">
+              <Form inline>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Label for="exampleEmail" className="mr-sm-2">Email</Label>
+                  <Input className="login1" placeholder="USERNAME" onChange={this.onChangeUsername} />
+                </FormGroup>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Label for="examplePassword" className="mr-sm-2">Password</Label>
+                  <Input className="login1" type="password" placeholder="PASSWORD" onChange={this.onChangePassword} />
+                </FormGroup>
+                <Button className="login1" color ="lightgray" onClick={() => this.onLoginPressed()}>SIGNIN</Button>
+                <Button className="singup" color ="lightgray">SIGNUP</Button>
+              </Form>
+              {/*<Input className="login1" placeholder="USERNAME" onChange={this.onChangeUsername} />*/}
+            {/*</CardBody>*/}
+            {/*<CardBody>*/}
+              {/*<Input className="login1" type="password" placeholder="PASSWORD" onChange={this.onChangePassword} />*/}
+            {/*</CardBody>*/}
+            {/*<CardBody>*/}
+              {/*/!*<CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>*!/*/}
+              {/*<Button className="login1" color ="lightgray" onClick={() => this.onLoginPressed()}>LOGIN</Button>*/}
+            </CardBody>
+          </Card>
           </Col>
         </Row>
       </Container>
